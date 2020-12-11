f79d2919417c7e390de7bd5d8c07b049f7a1a85a
EzzatEissa <ezzat.a.eissa@gmail.com>
2020-12-11 15:10:40 +0300

    swagger generator

--- src/main/java/com/sbm/dealer/controller/DealerController.java
+++ src/main/java/com/sbm/dealer/controller/DealerController.java
@@ -1,46 +1,58 @@
 package com.sbm.dealer.controller;
 
-import java.util.List;
-
-import javax.validation.Valid;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.DELETE;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import com.sbm.dealer.common.exception.GenericExceptionMapper;
 import com.sbm.dealer.dto.DealerDto;
 import com.sbm.dealer.dto.ResultDto;
 import com.sbm.dealer.service.DealerService;
+import io.swagger.annotations.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
-@Service
+import javax.validation.Valid;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.List;
+
+@Api("Dealers")
+@Component
 @Path("/dealer")
+@ApiResponses({
+        @ApiResponse(code = 400, message = "Bad request", responseHeaders = { @ResponseHeader(name = "x-fapi-interaction-id", description = "An RFC4122 UID used as a correlation id.", response = String.class) }, response = GenericExceptionMapper.class),
+        @ApiResponse(code = 401, message = "Unauthorized", responseHeaders = { @ResponseHeader(name = "x-fapi-interaction-id", description = "An RFC4122 UID used as a correlation id.", response = String.class) }),
+        @ApiResponse(code = 403, message = "Forbidden", responseHeaders = { @ResponseHeader(name = "x-fapi-interaction-id", description = "An RFC4122 UID used as a correlation id.", response = String.class) }, response = GenericExceptionMapper.class),
+        @ApiResponse(code = 404, message = "Not found", responseHeaders = { @ResponseHeader(name = "x-fapi-interaction-id", description = "An RFC4122 UID used as a correlation id.", response = String.class) }),
+        @ApiResponse(code = 405, message = "Method Not Allowed", responseHeaders = { @ResponseHeader(name = "x-fapi-interaction-id", description = "An RFC4122 UID used as a correlation id.", response = String.class) }, response = GenericExceptionMapper.class),
+        @ApiResponse(code = 406, message = "Not Acceptable", responseHeaders = { @ResponseHeader(name = "x-fapi-interaction-id", description = "An RFC4122 UID used as a correlation id.", response = String.class) }),
+        @ApiResponse(code = 415, message = "Unsupported Media Type", responseHeaders = { @ResponseHeader(name = "x-fapi-interaction-id", description = "An RFC4122 UID used as a correlation id.", response = String.class) }),
+        @ApiResponse(code = 429, message = "Too Many Requests", responseHeaders = { @ResponseHeader(name = "Retry-After", description = "Number in seconds to wait", response = Integer.class),
+                @ResponseHeader(name = "x-fapi-interaction-id", description = "An RFC4122 UID used as a correlation id.", response = String.class) }),
+        @ApiResponse(code = 500, message = "Internal Server Error", responseHeaders = {
+                @ResponseHeader(name = "x-fapi-interaction-id", description = "An RFC4122 UID used as a correlation id.", response = String.class) }, response = GenericExceptionMapper.class),
+        @ApiResponse(code = 503, message = "Service Unavailable", responseHeaders = {
+                @ResponseHeader(name = "x-fapi-interaction-id", description = "An RFC4122 UID used as a correlation id.", response = String.class) }, response = GenericExceptionMapper.class), })
 public class DealerController {
 
 
     @Autowired
     private DealerService dealerService;
 
+    @ApiOperation("Get All Dealers")
     @GET
     @Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+    @ApiResponses({ @ApiResponse(code = 200, message = "Get All Dealers", responseHeaders = {
+            @ResponseHeader(name = "x-fapi-interaction-id", description = "An RFC4122 UID used as a correlation id.", response = String.class) }, response = List.class) })
     public Response getAllDealers() {
         return Response.status(200).entity(dealerService.getAllDealers()).build();
     }
 
+    @ApiOperation("Create Dealer details")
     @POST
     @Consumes(MediaType.APPLICATION_JSON + ";charset=utf-8")
     @Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
-    public Response createDealer(DealerDto dealerDto) throws GenericExceptionMapper{
+    @ApiResponses({ @ApiResponse(code = 200, message = "Dealer Created Successfully", responseHeaders = {
+            @ResponseHeader(name = "x-fapi-interaction-id", description = "An RFC4122 UID used as a correlation id.", response = String.class) }, response = ResultDto.class) })
+    public Response createDealer(@Valid  DealerDto dealerDto) throws GenericExceptionMapper{
         dealerService.saveDealer(dealerDto);
         ResultDto resultDto = new ResultDto();
         resultDto.setCode("201");
@@ -49,18 +61,24 @@ public class DealerController {
         return Response.status(201).entity(resultDto).build();
     }
 
+    @ApiOperation("Get Dealer By Id")
     @GET
     @Path("/{id}")
     @Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
-    public Response getDealerById(@PathParam("id") Long id) throws GenericExceptionMapper{
+    @ApiResponses({ @ApiResponse(code = 200, message = "Get Dealer Info By Id", responseHeaders = {
+            @ResponseHeader(name = "x-fapi-interaction-id", description = "An RFC4122 UID used as a correlation id.", response = String.class) }, response = DealerDto.class) })
+    public Response getDealerById(@ApiParam(value = "id", required = true) @PathParam("id") Long id) throws GenericExceptionMapper{
         return Response.status(200).entity(dealerService.getDealerById(id)).build();
     }
 
+    @ApiOperation("Update Dealer By Id")
     @PUT
     @Consumes(MediaType.APPLICATION_JSON + ";charset=utf-8")
     @Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
     @Path("/{id}")
-    public Response updateDealer(@PathParam("id") Long id, @Valid DealerDto dealerDto) throws GenericExceptionMapper{
+    @ApiResponses({ @ApiResponse(code = 200, message = "Update Dealer Info By Id", responseHeaders = {
+            @ResponseHeader(name = "x-fapi-interaction-id", description = "An RFC4122 UID used as a correlation id.", response = String.class) }, response = ResultDto.class) })
+    public Response updateDealer(@ApiParam(value = "id", required = true) @PathParam("id") Long id, DealerDto dealerDto) throws GenericExceptionMapper{
         dealerDto.setId(id);
         dealerService.updateDealer(dealerDto);
         ResultDto resultDto = new ResultDto();
@@ -70,10 +88,13 @@ public class DealerController {
         return Response.status(200).entity(resultDto).build();
     }
 
+    @ApiOperation("Delete Dealer By Id")
     @DELETE
     @Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
     @Path("/{id}")
-    public Response deleteDealer(@PathParam("id") Long id) throws GenericExceptionMapper {
+    @ApiResponses({ @ApiResponse(code = 204, message = "Delete Dealer By Id", responseHeaders = {
+            @ResponseHeader(name = "x-fapi-interaction-id", description = "An RFC4122 UID used as a correlation id.", response = ResultDto.class) }) })
+    public Response deleteDealer(@ApiParam(value = "id", required = true) @PathParam("id") Long id) throws GenericExceptionMapper {
         dealerService.deleteDealer(id);
         ResultDto resultDto = new ResultDto();
         resultDto.setCode("200");
@@ -81,10 +102,13 @@ public class DealerController {
         resultDto.setType("success");
         return Response.status(200).entity(resultDto).build();
     }
-    
+
+    @ApiOperation("Search For Dealers By Any Fields")
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     @Path("/search")
+    @ApiResponses({ @ApiResponse(code = 200, message = "Search For Dealers By Any Fields", responseHeaders = {
+            @ResponseHeader(name = "x-fapi-interaction-id", description = "An RFC4122 UID used as a correlation id.", response = String.class) }, response = List.class) })
     public Response getVehiclesByFields(@QueryParam("dealerName") String dealerName,
                                         @QueryParam("dealerCode") String dealerCode,
                                         @QueryParam("preferredDealer") String preferredDealer,
--- src/main/java/com/sbm/dealer/dto/DealerDto.java
+++ src/main/java/com/sbm/dealer/dto/DealerDto.java
@@ -1,20 +1,31 @@
 package com.sbm.dealer.dto;
 
 import com.sbm.dealer.common.dto.BaseDto;
-import com.sbm.dealer.model.Dealer;
 import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Data
 public class DealerDto extends BaseDto {
 
+    @Size(min = 1, max = 30)
+    @NotBlank
     private String dealerName;
 
+    @Size(min = 1, max = 30)
+    @NotBlank
     private String dealerCode;
 
+    @Size(min = 1, max = 15)
+    @NotBlank
     private String language;
 
+    @Size(min = 1, max = 100)
     private String preferredDealer;
 
+    @Size(min = 1, max = 100)
     private String salesPartner;
 
     private Boolean usedCategory;
